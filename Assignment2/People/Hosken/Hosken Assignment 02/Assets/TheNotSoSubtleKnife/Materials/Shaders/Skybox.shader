afbc1744374e6ff98c6c3ce3d4553b7710b29d58
James Hosken <jameswfhosken@gmail.com>
2020-02-10 16:12:10 -0500

    Add Assignment 2

---
+++ Assignment2/People/Hosken/Hosken Assignment 02/Assets/TheNotSoSubtleKnife/Materials/Shaders/Skybox.shader
@@ -0,0 +1,115 @@
+﻿Shader "Custom/GasGiant"
+{
+	Properties
+	{
+		_Color("Color", Color) = (1,1,1,1)
+		_MainTex("Albedo (RGB)", 2D) = "white" {}
+		_Glossiness("Smoothness", Range(0,1)) = 0.5
+		_Metallic("Metallic", Range(0,1)) = 0.0
+
+		_NoiseScale("Noise Scale", Range(0.0001, 100)) = 1.0
+		_Octaves("Octaves", Range(1,10)) = 1
+		_Lacunarity("Lacunarity", Range(1,2)) = 1.5
+		_Persistence("Persistence", Range(0,1)) = 0.5
+		_Power("Power", Range(0,20)) = 1
+		
+
+
+	}
+		SubShader
+		{
+			Tags { "RenderType" = "Opaque" }
+			LOD 200
+
+			//THIS IS WHAT ALLOWS STENCILLING
+			Stencil {
+				Ref 1
+				Comp Equal
+			}
+
+
+
+			CGPROGRAM
+			// Physically based Standard lighting model, and enable shadows on all light types
+			#pragma surface surf Standard vertex:vert 
+
+			// Use shader model 3.0 target, to get nicer looking lighting
+			#pragma target 3.0
+
+			#include "Noise.hlsl"
+
+			sampler2D _MainTex;
+
+			struct Input
+			{
+				float2 uv_MainTex;
+				float3 localPos;
+			};
+
+			half _Glossiness;
+			half _Metallic;
+			half _NoiseScale;
+
+			int _Octaves;
+			half _Lacunarity;
+			half _Persistence;
+			half _Power;
+			fixed4 _Color;
+
+			// Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+			// See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+			// #pragma instancing_options assumeuniformscaling
+			//UNITY_INSTANCING_BUFFER_START(Props)
+			//	// put more per-instance properties here
+			//UNITY_INSTANCING_BUFFER_END(Props)
+
+			void vert(inout appdata_full v, out Input o) {
+				UNITY_INITIALIZE_OUTPUT(Input, o);
+				o.localPos = v.vertex.xyz;
+			}
+
+			half oct_noise(fixed3 pos, half scl, int oct, half lac, half per) {
+
+				half noise_val = 0;
+				half amp = 1;
+
+				
+				for (int i = 0; i < oct; i++) {
+
+					noise_val += snoise(pos * scl) * amp;
+					scl *= lac;
+					amp *= per;
+
+				}
+
+				noise_val /= oct;
+				noise_val = (noise_val + 1) / 2;
+
+				return noise_val;
+			}
+
+			void surf(Input IN, inout SurfaceOutputStandard o)
+			{
+
+				fixed3 sample_pos = IN.localPos ;
+					
+				half turbulence = oct_noise(IN.localPos, 10, 4, 1.5, 0.9);
+				sample_pos += pow(turbulence, 2) * 0.01;
+
+				half noise_val = oct_noise(sample_pos, _NoiseScale, _Octaves, _Lacunarity, _Persistence);
+				
+
+				// Albedo comes from a texture tinted by color
+				fixed4 c = tex2D(_MainTex, IN.uv_MainTex) * _Color * noise_val;
+				//o.Albedo = float3(0, 0, 0);
+				// Metallic and smoothness come from slider variables
+				o.Metallic = _Metallic;
+				o.Smoothness = _Glossiness;
+				o.Alpha = c.a;
+				o.Emission = pow(c.rgb, _Power);
+
+			}
+			ENDCG
+		}
+			FallBack "Diffuse"
+}
---
+++ Assignment2/People/Hosken/Hosken Assignment 02/Assets/TheNotSoSubtleKnife/Scripts/WindowController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowController : MonoBehaviour
+{
+
+    [SerializeField] GameObject handleTemplate;
+
+    //Handles created clockwise starting top left;
+    GameObject[] handles = new GameObject[4];
+
+    Vector3 referenceUp;
+    Vector3 referneceTan;
+
+    MeshRenderer renderer;
+    MeshFilter filter;
+    Mesh mesh;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        renderer = GetComponent<MeshRenderer>();
+        filter = GetComponent<MeshFilter>();
+        mesh = new Mesh();
+        filter.mesh = mesh;
+    }
+
+    public GameObject PlaceFirstHandle(Vector3 worldPos, Vector3 up)
+    {
+
+        referenceUp = EstimateUpVec(up);
+
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject handle = Instantiate(handleTemplate, transform, false);
+            handle.transform.position = worldPos;
+            handles[i] = handle;
+        }
+
+        // Return which handle to move when first placed
+        return handles[2];
+    }
+
+    
+
+    public void UpdateHandlePosition(GameObject handle, Vector3 position)
+    {
+        int idx = GetHandleIndex(handle);
+
+        //Find opposite handle:
+        int lockedIdx = (idx + 2) % 4;
+        GameObject locked = handles[lockedIdx];
+
+        handle.transform.position = position;
+
+        Vector3 diagonal = handle.transform.position - locked.transform.position;
+
+        Vector3 normal = Vector3.Cross(referenceUp, diagonal).normalized;
+
+        Vector3 tangent = Vector3.Cross(referenceUp, normal).normalized;
+
+        //Fix this clunky nonesense
+        float direction = Mathf.Sign(Vector3.Dot(normal, Camera.main.transform.forward));
+        //direction = direction * Mathf.Sign(Vector3.Dot(referenceUp, locked.transform.position - handle.transform.position));
+
+        // Find adjacent corners
+        handles[(idx - 1) % 4].transform.position = locked.transform.position + Vector3.Scale(diagonal, referenceUp);
+
+        handles[(idx + 1) % 4].transform.position = locked.transform.position + Vector3.Scale(diagonal, tangent) * direction;
+
+        UpdateMesh();
+
+    }
+
+    
+
+    public int GetHandlePosition(GameObject obj) {
+        if(obj == handles[0])
+        {
+            return 0;
+        }else if (obj == handles[1])
+        {
+            return 1;
+        }
+        else if (obj == handles[2])
+        {
+            return 2;
+        }
+        else if (obj == handles[3])
+        {
+            return 3;
+        }
+
+        return -1;
+    }
+
+    void UpdateMesh()
+    {
+        /* Four tris (2 per side): 
+            0, 1, 3
+            1, 2, 3
+            0, 3, 1
+            1, 3, 2
+        */
+
+        Vector3 a = handles[0].transform.position;
+        Vector3 b = handles[1].transform.position;
+        Vector3 c = handles[2].transform.position;
+        Vector3 d = handles[3].transform.position;
+
+        mesh.Clear();
+
+        mesh.vertices = new Vector3[]{a,b,c,d};
+        mesh.triangles = new int[] {
+            0, 1, 3,
+            1, 2, 3,
+            0, 3, 1,
+            1, 3, 2
+        };
+
+        mesh.RecalculateNormals();
+    }
+
+    /*
+     * 
+     * HELPERS
+     * 
+     */
+    private Vector3 EstimateUpVec(Vector3 up)
+    {
+        Vector3 payload;
+        //If up vector biggest, 
+        if ((up.y * up.y) > (up.x * up.x + up.z * up.z))
+        {
+            payload = Vector3.up;
+        }
+
+        else if ((up.x * up.x) > (up.y * up.y + up.z * up.z))
+        {
+            payload = Vector3.right;
+        }
+        else
+        {
+            payload = Vector3.forward;
+        }
+
+        return payload;
+    }
+
+    private int GetHandleIndex(GameObject handle)
+    {
+        int idx = -1;
+        for (int i = 0; i < 4; i++)
+        {
+            if (handles[i] == handle)
+            {
+                idx = i;
+            }
+        }
+        return idx;
+    }
+}
